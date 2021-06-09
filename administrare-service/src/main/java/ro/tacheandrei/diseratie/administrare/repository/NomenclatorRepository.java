e48b0291026ae48eafc9ffe41a3ca528f3895cba
Andrei Tache <tacheandrei96@gmail.com>
2021-06-10 01:32:08 +0300

    10.06.2021

--- administrare-service/src/main/java/ro/tacheandrei/diseratie/administrare/repository/NomenclatorRepository.java
+++ administrare-service/src/main/java/ro/tacheandrei/diseratie/administrare/repository/NomenclatorRepository.java
@@ -3,13 +3,16 @@ package ro.tacheandrei.diseratie.administrare.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 import ro.tacheandrei.diseratie.administrare.domain.Nomenclator;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
 
-public interface NomenclatorRepository extends JpaRepository<Nomenclator, Long> , PagingAndSortingRepository<Nomenclator, Long>, JpaSpecificationExecutor<Nomenclator> {
+@Repository
+public interface NomenclatorRepository extends JpaRepository<Nomenclator, Long> , PagingAndSortingRepository<Nomenclator, Long>,
+        JpaSpecificationExecutor<Nomenclator> {
 
     Optional<Nomenclator> findByCod(String cod);
 
